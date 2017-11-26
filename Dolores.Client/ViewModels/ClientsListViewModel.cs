fe11c254fc2dde49895f6f0835ca95e70b059968
Vasyl Barna <vasaaa1993@gmail.com>
2017-11-26 19:32:49 +0200

    Added search panel to ClientListView

--- Dolores.Client/ViewModels/ClientsListViewModel.cs
+++ Dolores.Client/ViewModels/ClientsListViewModel.cs
@@ -57,7 +57,7 @@ namespace Dolores.Client.ViewModels
 				},
 				new ClientDto()
 				{
-					FirstName = "Василь",
+					FirstName = "Андрій",
 					SecondName = "Барна",
 					MiddleName = "Олегович",
 					Region = "Тернопільська",
@@ -98,9 +98,181 @@ namespace Dolores.Client.ViewModels
 						}
 					}
 				},
-			    new ClientDto()
+				new ClientDto()
+				{
+					FirstName = "Олег",
+					SecondName = "Барна",
+					MiddleName = "Олегович",
+					Region = "Тернопільська",
+					District = "Чортківчький",
+					Town = "Білобожниця",
+					Street = "Шевченка",
+					Building = "5",
+					Apartment = "",
+
+					Email = "vasaaa1993@gmail.com",
+					LastContactTime = DateTime.Now,
+					DateOfContract = DateTime.Now,
+					GasSealNumber = "134454354",
+					GasServiceContractNumber = "321321321",
+					ContractNumber = "65465465",
+
+					Description = "Дуже крутий чувак",
+					Phones = new ObservableCollection<PhoneDto>()
+					{
+						new PhoneDto()
+						{
+							Number = "0932266519"
+						},
+						new PhoneDto()
+						{
+							Number = "093226520"
+						}
+					},
+					Equimpents = new ObservableCollection<EquipmentParamDto>()
+					{
+						new EquipmentParamDto()
+						{
+							Name = "Інструкція",
+						},
+						new EquipmentParamDto()
+						{
+							Name = "Специфікація"
+						}
+					}
+				},
+				new ClientDto()
+				{
+					FirstName = "Марк",
+					SecondName = "Барна",
+					MiddleName = "Олегович",
+					Region = "Тернопільська",
+					District = "Чортківчький",
+					Town = "Білобожниця",
+					Street = "Шевченка",
+					Building = "5",
+					Apartment = "",
+
+					Email = "vasaaa1993@gmail.com",
+					LastContactTime = DateTime.Now,
+					DateOfContract = DateTime.Now,
+					GasSealNumber = "134454354",
+					GasServiceContractNumber = "321321321",
+					ContractNumber = "65465465",
+
+					Description = "Дуже крутий чувак",
+					Phones = new ObservableCollection<PhoneDto>()
+					{
+						new PhoneDto()
+						{
+							Number = "0932266519"
+						},
+						new PhoneDto()
+						{
+							Number = "093226520"
+						}
+					},
+					Equimpents = new ObservableCollection<EquipmentParamDto>()
+					{
+						new EquipmentParamDto()
+						{
+							Name = "Інструкція",
+						},
+						new EquipmentParamDto()
+						{
+							Name = "Специфікація"
+						}
+					}
+				},
+				new ClientDto()
+				{
+					FirstName = "Роман",
+					SecondName = "Барна",
+					MiddleName = "Олегович",
+					Region = "Тернопільська",
+					District = "Чортківчький",
+					Town = "Білобожниця",
+					Street = "Шевченка",
+					Building = "5",
+					Apartment = "",
+
+					Email = "vasaaa1993@gmail.com",
+					LastContactTime = DateTime.Now,
+					DateOfContract = DateTime.Now,
+					GasSealNumber = "134454354",
+					GasServiceContractNumber = "321321321",
+					ContractNumber = "65465465",
+
+					Description = "Дуже крутий чувак",
+					Phones = new ObservableCollection<PhoneDto>()
+					{
+						new PhoneDto()
+						{
+							Number = "0932266519"
+						},
+						new PhoneDto()
+						{
+							Number = "093226520"
+						}
+					},
+					Equimpents = new ObservableCollection<EquipmentParamDto>()
+					{
+						new EquipmentParamDto()
+						{
+							Name = "Інструкція",
+						},
+						new EquipmentParamDto()
+						{
+							Name = "Специфікація"
+						}
+					}
+				},
+				new ClientDto()
+				{
+					FirstName = "Володимир",
+					SecondName = "Барна",
+					MiddleName = "Олегович",
+					Region = "Тернопільська",
+					District = "Чортківчький",
+					Town = "Білобожниця",
+					Street = "Шевченка",
+					Building = "5",
+					Apartment = "",
+
+					Email = "vasaaa1993@gmail.com",
+					LastContactTime = DateTime.Now,
+					DateOfContract = DateTime.Now,
+					GasSealNumber = "134454354",
+					GasServiceContractNumber = "321321321",
+					ContractNumber = "65465465",
+
+					Description = "Дуже крутий чувак",
+					Phones = new ObservableCollection<PhoneDto>()
+					{
+						new PhoneDto()
+						{
+							Number = "0932266519"
+						},
+						new PhoneDto()
+						{
+							Number = "093226520"
+						}
+					},
+					Equimpents = new ObservableCollection<EquipmentParamDto>()
+					{
+						new EquipmentParamDto()
+						{
+							Name = "Інструкція",
+						},
+						new EquipmentParamDto()
+						{
+							Name = "Специфікація"
+						}
+					}
+				},
+				new ClientDto()
 			    {
-				    FirstName = "Василь",
+				    FirstName = "Ярослав",
 				    SecondName = "Барна",
 				    MiddleName = "Олегович",
 				    Region = "Тернопільська",
